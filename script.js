2f8f37c566440d4b023aa08198c6c50f61647440
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-07-19 13:20:35 +0200

    feat: add image search through API

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const contentDiv = document.getElementById("content");
-const buttonImg = document.getElementById("button-random-dog");
+const buttonRandom = document.getElementById("button-random-dog");
+const buttonBreed = document.getElementById("button-show-breed");
 
-let randomImage = document.createElement("img");
-contentDiv.appendChild(randomImage);
-buttonImg.addEventListener("click", randomImg);
- 
+let dogImage = document.createElement("img");
+buttonRandom.addEventListener("click", randomImg);
+buttonBreed.addEventListener("click", getBreed);
+
 async function randomImg() {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const dog = await response.json();
-    randomImage.setAttribute("src", dog.message);
+    dogImage.setAttribute("src", dog.message);
+    contentDiv.replaceChildren(dogImage);
+}
+
+async function getBreed() {
+    const breed = document.getElementById("input-breed").value.toLowerCase();
+    const response = await fetch("https://dog.ceo/api/breed/" + breed + "/images/random")
+    const dog = await response.json();
+    if (!response.ok) {
+        const errorText = document.createElement("p");
+        errorText.textContent = "Breed not found!";
+        contentDiv.replaceChildren(errorText);
+        return;
+    }
+    dogImage.setAttribute("src", dog.message);
+    contentDiv.replaceChildren(dogImage);
 }
